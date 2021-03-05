0849f8b6e27d7f9301dc18460fcd41f588263d09
ChillinPawel <pawel.jarubas@gmail.com>
2021-03-05 20:00:03 +0100

    First add to repository. App written in 2019.

---
+++ src/com/pawel/Main.java
@@ -0,0 +1,12 @@
+package com.pawel;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        QuizCardBuilder quizBuilder = new QuizCardBuilder();
+//        quizBuilder.go();
+
+        QuizCardPlayer quizPlayer = new QuizCardPlayer();
+        quizPlayer.go();
+    }
+}
